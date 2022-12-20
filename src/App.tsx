5f1bebedaae38424897c0068818e402fe8ca9188
Thomas <120879565+Thomas91Miller@users.noreply.github.com>
2022-12-19 21:58:12 -0500

    Create App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,44 @@
+import { useLocalStorage, useColorScheme } from '@mantine/hooks';
+import { StyledHeader, Home } from './components';
+import * as React from 'react';
+import { ColorSchemeProvider, MantineProvider, ColorScheme, AppShell } from '@mantine/core';
+
+type Props = {}
+
+export const App: React.FC<Props> = (_: Props) => {
+  const [colorScheme, setColorScheme] = useLocalStorage<ColorScheme>({
+    defaultValue: useColorScheme(),
+    key: 'theme',
+  });
+
+  const toggleColorScheme = (value?: ColorScheme) => setColorScheme(value || (colorScheme === 'dark' ? 'light' : 'dark'));
+    
+  const dark = colorScheme === 'dark';
+
+  return (
+    <ColorSchemeProvider
+      colorScheme={colorScheme}
+      toggleColorScheme={toggleColorScheme}
+    >
+      <MantineProvider
+        theme={{
+          primaryColor: dark ? 'yellow' : 'blue',
+          colorScheme,
+        }}
+        withGlobalStyles
+        withNormalizeCSS
+      >
+        <AppShell
+          header={<StyledHeader />}
+          styles={{
+            main: {
+              padding: '60px 0 0 0',
+            },
+          }}
+        >
+          <Home />
+        </AppShell>
+      </MantineProvider>
+    </ColorSchemeProvider>
+  );
+}
